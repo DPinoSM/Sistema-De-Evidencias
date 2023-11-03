c6d05a54f3cba1aadf1085153c554bbf0b9e99ea
Yoni <83724463+Yoni1999@users.noreply.github.com>
2023-11-03 11:05:11 -0300

    yoni

--- server/dist/models/detalle_revisor.js
+++ server/dist/models/detalle_revisor.js
@@ -1,20 +1,34 @@
-//Jonathan Molina Gonzalez
-//Modelo detalle_revisor
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DetalleRevisor = void 0;
+exports.Detalle_Revisor = void 0;
+//JONATHAN MOLINA 
+//MODELS DETALLE REVISOR
 const sequelize_1 = require("sequelize");
 const connection_1 = __importDefault(require("../db/connection"));
-exports.DetalleRevisor = connection_1.default.define('detalle_revisor', {
-    "id_detalle_Revisor": { type: sequelize_1.DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    "revisado_revisor": { type: sequelize_1.DataTypes.BOOLEAN },
-    "estado_revisor": {type: sequelize_1.DataTypes.BOOLEAN },
-    "comentario_revisor": {type: sequelize_1.DataTypes.STRING}
+exports.Detalle_Revisor = connection_1.default.define('detalle_revisor', {
+    "id_detalle_revisor": {
+        type: sequelize_1.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    "revisado_revisor": {
+        type: sequelize_1.DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    "estado_revisor": {
+        type: sequelize_1.DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    "comentario_revisor": {
+        type: sequelize_1.DataTypes.STRING
+    }
 }, {
     freezeTableName: true,
     timestamps: false,
 });
-//fin
+//FIN
