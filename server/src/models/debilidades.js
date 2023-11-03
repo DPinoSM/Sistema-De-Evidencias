e32a61661b35c301a8e5424de4880ce88b6e3a0f
Yoni <83724463+Yoni1999@users.noreply.github.com>
2023-11-03 11:08:21 -0300

    yoni

---
+++ server/src/models/debilidades.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Debilidades = void 0;
+//JONATHAN MOLINA 
+//MODELS DEBILIDADES
+const sequelize_1 = require("sequelize");
+const connection_1 = __importDefault(require("../db/connection"));
+const criterio_1 = require("./criterio");
+exports.Debilidades = connection_1.default.define('debilidades', {
+    "id_debilidades": {
+        type: sequelize_1.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    "descripcion_debilidades": {
+        type: sequelize_1.DataTypes.STRING(70)
+    },
+    "estado_debilidades": {
+        type: sequelize_1.DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    "id_criterios": {
+        type: sequelize_1.DataTypes.INTEGER
+    }
+}, {
+    freezeTableName: true,
+    timestamps: false,
+});
+//Establece la relación entre la clave primaria 
+exports.Debilidades.belongsTo(criterio_1.Criterio, { foreignKey: 'id_criterios' });
+//FIN
