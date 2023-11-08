23e09e51e657968400133b6075162eed8336cbde
NicolasPavz <npavez43@gmail.com>
2023-11-08 20:27:55 -0300

    token

--- server/dist/routes/validate-token.js
+++ server/dist/routes/validate-token.js
@@ -6,12 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const validateToken = (req, res, next) => {
     const headerToken = req.headers['authorization'];
-    console.log(headerToken);
     if (headerToken != undefined && headerToken.startsWith('Bearer ')) {
         // Tiene token
         try {
             const bearerToken = headerToken.slice(7);
-            console.log(bearerToken);
             jsonwebtoken_1.default.verify(bearerToken, process.env.SECRET_KEY || 'HS384');
             next();
         }
--- server/src/routes/validate-token.ts
+++ server/src/routes/validate-token.ts
@@ -3,13 +3,11 @@ import jwt from 'jsonwebtoken';
 
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
     const headerToken = req.headers['authorization'];
-    console.log(headerToken)
 
     if (headerToken != undefined && headerToken.startsWith('Bearer ')) {
         // Tiene token
         try {
             const bearerToken = headerToken.slice(7);
-            console.log(bearerToken)
             jwt.verify(bearerToken, process.env.SECRET_KEY || 'HS384');
             next();
         } catch (error) {
